9e5e62cea6c43de971ce457cd048d26ec42b1015
unknown <aliopdandan@gmail.com>
2024-06-20 09:18:21 +0100

    commit

--- server/server.js
+++ server/server.js
@@ -1,9 +1,24 @@
-const express = require("express")
-const app = express()
-const port = 5000
+const express = require("express");
+const app = express();
+const port = 5000;
+const mongoose = require("mongoose")
+const user = require("./models/userSchema")
 
-app.get('/api', (req, res) => {
-    res.json({message: "This is from the backend"})
+app.use(express.json())
+
+mongoose.connect("mongodb://127.0.0.1:27017/usersDatabase", )
+.then(() => console.log("Database connected"))
+.catch((Error) => console.log({message: Error}))
+
+app.post("/api/users", async (req, res) => {
+    try {
+        const {username, email, password} = req.body;
+        const newUser = new user({userName: username, Password: password, email: email });
+        console.log(newUser);
+        const saveUser = await newUser.save();
+    } catch (error) {
+        res.json({Message: error})
+    }
 })
 
 app.listen(port, console.log(`listent to port ${port}`))
